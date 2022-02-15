f73625b8d8defaf3ea0cee5aa8c289eb34dff78e
YeongJin-Jo <yjjo97@naver.com>
2022-02-15 21:31:42 +0900

    유니티 입문 프로젝트 ~ Part 14

---
+++ Part 12 - Slider of UI/Assets/Scripts/SliderOfUI_Unity.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class SliderOfUI_Unity : MonoBehaviour
+{
+    [SerializeField] private Slider slider;
+
+    private bool isClick;
+
+    private float dotTime = 1f;
+    private float currentDotTime = 0f;
+
+    void Start()
+    {
+        currentDotTime = dotTime;
+    }
+
+    void Update() 
+    {
+        if(isClick)
+        {
+            currentDotTime -= Time.deltaTime;
+
+            if(currentDotTime <= 0)
+            {
+                slider.value -= Time.deltaTime;
+
+                if(currentDotTime <= -1f)
+                {
+                    currentDotTime = dotTime;
+                }
+            }
+            
+        }
+    }
+    
+    public void Button() 
+    {
+        isClick = true;
+        slider.value -= 1;
+    }
+}
