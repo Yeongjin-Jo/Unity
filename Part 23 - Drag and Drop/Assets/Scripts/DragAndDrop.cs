0165fda342e830215c5e8f10911941a58523bd0b
YeongJin-Jo <yjjo97@naver.com>
2022-02-21 18:12:29 +0900

    유니티 입문 프로젝트 ~ Part 26

---
+++ Part 23 - Drag and Drop/Assets/Scripts/DragAndDrop.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragAndDrop : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IDropHandler
+{
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("Begin Drag");
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = eventData.position;
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log("Drop");
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("End Drag");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
